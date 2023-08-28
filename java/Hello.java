acfeb3c6ba39e4c36b8c3f5d1e389e50a3465832
Akash kumar <akash.kumar@mobisec.in>
2023-08-28 15:23:43 +0530

    new file

--- java/Hello.java
+++ java/Hello.java
@@ -2,5 +2,6 @@ class Hello(){
     public static void main(String arg[]){
         System.out.println("Hello World");
         System.out.println("Hello Moon");
+        System.out.println("Hello Moon")
     }
 }
