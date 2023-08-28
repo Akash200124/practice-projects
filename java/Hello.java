1f0fab9b1dd47d13054e4747d404931f2189bbcd
Akash kumar <akash.kumar@mobisec.in>
2023-08-28 15:56:37 +0530

    adding sum file

--- java/Hello.java
+++ java/Hello.java
@@ -1,10 +1,10 @@
-class Hello(){
+class Hello{
     public static void main(String arg[]){
         System.out.println("Hello World");
         System.out.println("Hello Moon");
         System.out.println("Hello Moon");
         System.out.println("Hello Moon");
         System.out.println("Hello");
-        
+
     }
 }
---
+++ java/sum.java
@@ -0,0 +1,13 @@
+class sum
+{
+
+    public static void main (String args[])
+    {
+
+        int a =29;
+        int b = 39;
+    
+        System.out.println("sum of a and b is " + a+b);
+    }
+
+}
