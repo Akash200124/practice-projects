73a6589d278d555d816e33e915202b7a4a9c1a94
Akash kumar <akash.kumar@mobisec.in>
2023-08-28 16:00:52 +0530

    adding multi file

---
+++ java/multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+    
+
+    public static void main (String args[])
+    {
+        int a =34;
+        int b =30;
+        System.out.println("multiplication of a and b is " + a*b );
+
+    }
+}
